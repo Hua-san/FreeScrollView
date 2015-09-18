9a1738763166b8101bcef552ea7e4f39d8662e89
Mac <Mac@hua-san.local>
2015-09-18 16:34:21 +0800

    初始化项目

---
+++ LoopScrollView/LoopScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoopScrollView
+//
+//  Created by Mac on 15/9/18.
+//  Copyright © 2015年 huagangwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoopScrollView/LoopScrollView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LoopScrollView
+//
+//  Created by Mac on 15/9/18.
+//  Copyright © 2015年 huagangwan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"测试");
+}
+
+@end
